23b9ef2e218dac2c5961dde10c4fa480fe1a95e3
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-03 23:45:43 +0500

    server side auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,45 @@
-export default function Home() {
-  return <></>;
-}
+import axios from "axios";
+
+type IUser = {
+  user: {
+    name: string;
+    id: number;
+    userId: number;
+    email: string;
+    token: string;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+};
+
+export const getServerSideProps = async () => {
+  let user: IUser | undefined;
+  await axios
+    .post("http://localhost:3333/api/users/signIn", {
+      email: "bab@gmail.com",
+      password: "Aydogdy@2022",
+    })
+    .then((res) => {
+      user = res.data.user;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  return {
+    props: { user },
+  };
+};
+
+const Home = ({ user }: IUser) => {
+  // Show the user. No loading state is required
+  return (
+    <>
+      <h1>Your Profile</h1>
+      <pre>name: {user.name}</pre>
+      <pre>email: {user.email}</pre>
+    </>
+  );
+};
+
+export default Home;
