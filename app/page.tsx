e8bd542007ce489f28e804fa9802d6a452145242
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-06 18:07:00 +0500

    post schema db example

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
 
-export default function Home() {
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
+  if (!res.ok) {
+    console.log(res);
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data: { id: number; title: string }[] = await getPosts();
+  console.log(data);
   return (
     <main className="py-4 px-48">
       <Link
@@ -9,6 +20,10 @@ export default function Home() {
       >
         goto dashboard
       </Link>
+
+      {data.map((post) => (
+        <h1>{post.title}</h1>
+      ))}
     </main>
   );
 }
